84054b6ae2615f49367bd4b5e87758cc27bf752c
Devchai072 <devchai072@gmail.com>
2020-04-13 02:28:26 +0700

    Updates

---
+++ precache-manifest.ddba0486cfe05a54c72fd96cddd68a8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0bd2cfac3099bbb22185f99e2e8e70f",
+    "url": "/react-kendo-ui/index.html"
+  },
+  {
+    "revision": "7a14f1ff85993e06934f",
+    "url": "/react-kendo-ui/static/css/2.161baf39.chunk.css"
+  },
+  {
+    "revision": "97a02d122bd49583bb02",
+    "url": "/react-kendo-ui/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7a14f1ff85993e06934f",
+    "url": "/react-kendo-ui/static/js/2.9269cd51.chunk.js"
+  },
+  {
+    "revision": "d455b55662ec59e842e850740eeaf3d2",
+    "url": "/react-kendo-ui/static/js/2.9269cd51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97a02d122bd49583bb02",
+    "url": "/react-kendo-ui/static/js/main.8fe56045.chunk.js"
+  },
+  {
+    "revision": "5c4663f87aecab0a3d3e",
+    "url": "/react-kendo-ui/static/js/runtime-main.2733c8e0.js"
+  }
+]);
